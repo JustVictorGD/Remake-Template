a0b355e525aaf7cadf4477f3e57f7d7dc0f02c29
poec987 <poedev08@outlook.com>
2024-03-07 16:35:11 -0600

    Texture change depending on speed

--- gdscripts/speed_changer.gd
+++ gdscripts/speed_changer.gd
@@ -3,5 +3,13 @@ extends Area2D
 @export_category("Object Settings")
 @export var speed = 4 # 4 is the default player speed.
 
+func _ready():
+	if speed > 4:
+		$Texture.texture = load("res://pngs and svgs/speed_changer_placeholder-fast.png")
+	if speed == 4:
+		$Texture.texture = load("res://pngs and svgs/speed_changer_placeholder-normal.png")
+	if speed < 4:
+		$Texture.texture = load("res://pngs and svgs/speed_changer_placeholder.png")
+
 func touched_by_player():
 	GlobalSignal.speed_changed.emit(speed)
